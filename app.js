bc265cff34a30c2d71d31707e07e34f0b999e1ea
mpagung <michaelputra.agung@gmail.com>
2021-02-19 11:33:30 +0800

    finish building API

--- app.js
+++ app.js
@@ -24,11 +24,13 @@ const Article= mongoose.model("Article",articleSchema);
 app.route("/articles")
 
   .get(function(req,res){
-    if (!err){
-      res.send(foundArticles);
-    } else {
-      res.send(err);
-    }
+    Article.find({},function(err,foundArticles){
+      if (foundArticles){
+        res.send(foundArticles);
+      } else {
+        res.send(err);
+      }
+    });
   })
 
   .post(function(req,res){
@@ -56,9 +58,52 @@ app.route("/articles")
       });
   });
 
-app.post("/articles/:articleName",function(req,res){
+app.route("/articles/:articleTitle")
+  .get(function(req,res){
 
-})
+    Article.findOne({title:req.params.articleTitle},function(err,foundArticle){
+      if(foundArticle){
+        res.send(foundArticle);
+      } else {
+        res.send("No such article was found");
+      }
+    });
+  })
+
+  .put(function(req,res){
+    Article.update(
+      {title:req.params.articleTitle},
+      {title:req.body.title, content:req.body.content},
+      {overwrite:true},
+    function(err){
+      if(!err){
+        res.send("Successfully updated article");
+      } else {res.send(err);}});
+  })
+
+  .patch(function(req,res){
+    Article.update(
+      {title:req.params.articleTitle},
+      {$set: req.body},
+      function(err){
+        if (!err){
+        res.send("Successfully updated article");
+      } else {res.send(err);}
+    });
+  })
+
+  .delete(function(req,res){
+    Article.deleteOne(
+      {title:req.params.articleTitle},
+      function(err){
+        if(!err){
+          res.send("Successfully deleted the selected article");
+        } else {
+          res.send(err);
+        }
+
+      });
+  });
 
 app.listen(3000,function(){
   console.log("Server started on port 3000")
